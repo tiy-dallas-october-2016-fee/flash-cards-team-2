58e91ab73220f8c19ab6999c377f7012ad58c0c5
Tatrakrad <riddick.taylor@gmail.com>
2017-01-12 14:28:25 -0600

    sort working with proper buttons. card submission done button has error from validation branch

--- client/js/components/SetListComponent.js
+++ client/js/components/SetListComponent.js
@@ -44,6 +44,48 @@ if (window.FC === undefined) { window.FC = {}; }
       ReactRouter.hashHistory.push('/set/' + setId + '/quizzer');
     }
 
+    alphabetizeSets(){
+      var sortSets = this.state.sets;
+      //test
+      console.log("alphabetize set",sortSets);
+      //
+      sortSets.sort(function(a,b){
+
+        if (a.name < b.name){
+          return -1;
+        }
+        if (a.name > b.name){
+          return 1;
+        }
+        return 0;
+
+      })
+
+      this.setState({
+       sets:sortSets,
+       sort:"alpha"
+      })
+    }
+
+    sizeOrderSets(){
+      var sortSets = this.state.sets;
+
+      sortSets.sort(function(a,b){
+        if (a.cards.length < b.cards.length){
+          return 1;
+        }
+        if (a.cards.length > b.cards.length){
+          return -1;
+        }
+        return 0;
+      })
+
+      this.setState({
+        sets:sortSets,
+        sort:"number"
+      })
+    }
+
     render() {
       console.log('SetList.render', this.state);
 
@@ -52,8 +94,28 @@ if (window.FC === undefined) { window.FC = {}; }
         noSetsMessaging = <p>You do not have any sets! Create one.</p>
       }
 
+      var activeAlpha = "inactive";
+      var activeNum = "inactive";
+      if (this.state.sort !==null){
+        if(this.state.sort ==='number'){
+          activeAlpha = 'inactive';
+          activeNum = 'active';
+        }
+        else if (this.state.sort ==='alpha') {
+          activeAlpha = 'active';
+          activeNum ='inactive';
+        }
+
+      }
+
       return <div className="set-list">
         <h2>Set List</h2>
+
+        <div className="sort">
+          <div className={activeAlpha} onClick={() => {this.alphabetizeSets();}}>by name</div>
+          <div className={activeNum} onClick={() => {this.sizeOrderSets();}}>by # of cards</div>
+        </div>
+
 
         {noSetsMessaging}
 
